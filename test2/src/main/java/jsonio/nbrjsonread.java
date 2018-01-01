86dfbcd58e893fc72cdbcec86a3069585754b909
lelin <jackle@lelins-MacBook-Pro.local>
2018-01-01 16:54:09 -0600

    debug

---
+++ test2/src/main/java/jsonio/nbrjsonread.java
@@ -0,0 +1,162 @@
+package jsonio;
+
+import java.util.Iterator;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.util.Iterator;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+public class nbrjsonread 
+{
+	
+	public static HashMap<String,HashMap<String,HashSet<String>>>  to_concept_node_nbr(JSONObject nbr) 
+	{
+		HashMap<String,HashMap<String,HashSet<String>>> concept_nodes_nbr =  new HashMap<String,HashMap<String,HashSet<String>>>();
+        try
+        {
+            JSONArray all_nodes = (JSONArray)nbr.get("nodes");
+            for (Object node : all_nodes)
+            {
+                JSONObject _node_nbr = (JSONObject) nbr.get((String)node);
+
+                JSONArray pro_nbr = (JSONArray)_node_nbr.get("problem");
+                JSONArray con_nbr = (JSONArray)_node_nbr.get("concept");
+
+                HashSet<String> problem_node = new HashSet<String>();
+                HashSet<String> concept_node = new HashSet<String>();
+                HashMap<String,HashSet<String>> node_nbr = new HashMap<String,HashSet<String>>();
+
+                for(Object problem : pro_nbr)
+					problem_node.add((String)problem);				}
+				node_nbr.put("problem",problem_node);
+
+				for(Object concept : con_nbr)
+					concept_node.add((String)concept);
+				node_nbr.put("concept",concept_node)
+
+     			concept_nodes_nbr.put((String)node,node_nbr);
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return concept_nodes_nbr;
+	}
+
+	public static HashMap<HashSet<String>,Double[][]> to_undirected_edges(JSONObject undir_edge)
+	{
+		HashMap<HashSet<String>,Double[][]> undirected_edges =  new HashMap<HashSet<String>,Double[][]>();
+        try
+        {
+            JSONArray all_edges = (JSONArray)undir_edge.get("edges");
+            for (Object edge : all_edges)
+            {
+                JSONArray _edge = (JSONArray) undir_edge.get((String)edge);
+
+                Double[][] potential = new Double[2][2];
+
+                int i = 0;
+                for (Object a : _edge)
+                {
+                	int j = 0;
+	                for (Object b : (JSONArray)a)
+	                {
+	                	potential[i][j] = (double)b;
+	                    j++;
+	                }
+	                i ++;
+                }
+
+     			undirected_edges.put((String)edge,potential);
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return undirected_edges;
+	}
+
+	public static HashMap<String,Double[]>  to_directed_edges(JSONObject dir_edge)
+	{
+		HashMap<HashSet<String>,Double[]> directed_edges =  new HashMap<HashSet<String>,Double[]>();
+
+        JSONArray all_edges = (JSONArray)dir_edge.get("edges");
+        for (Object edge : all_edges)
+        {
+            JSONArray _edge = (JSONArray) dir_edge.get((String)edge);
+
+            Double[] potential = new Double[2];
+
+            int i = 0;
+            for (Object a : _edge)
+            {
+            	potential[i] = (double)a;
+                i++;
+            }
+
+ 			directed_edges.put((String)edge,potential);
+        }
+
+        return directed_edges;
+
+	}
+
+	
+	public static void bptest() throws IOException 
+	{
+
+		JSONObject obj1 =  concept_node_nbr_toJSON (concept_nodes_nbr);
+		JSONObject obj2 =  undirected_edges_toJSON (undirected_edges);
+		JSONObject obj3 =  directed_edges_toJSON (directed_edges);
+		
+		FileWriter file1 = new FileWriter("jsonfile1.txt");
+		FileWriter file2 = new FileWriter("jsonfile2.txt");
+		FileWriter file3 = new FileWriter("jsonfile3.txt");
+
+		file1.write(obj1.toJSONString());
+		file1.flush();
+		file1.close();
+		System.out.println("Successfully Copied JSON Object to File...");
+		System.out.println("\nJSON Object: " + obj1);
+		file2.write(obj2.toJSONString());
+		file2.flush();
+		file2.close();
+		System.out.println("Successfully Copied JSON Object to File...");
+		System.out.println("\nJSON Object: " + obj2);
+		file3.write(obj3.toJSONString());
+		file3.flush();
+		file3.close();
+		System.out.println("Successfully Copied JSON Object to File...");
+		System.out.println("\nJSON Object: " + obj3);
+
+		
+	}
+	
+	public static void main(String[] acg) 
+	{
+		try {bptest();}
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+}
